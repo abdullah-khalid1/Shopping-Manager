61c87f448a165cc3dac866ee1638efb4ac7c59fb
Abdullah Khalid <91566551+abdullah-khalid1@users.noreply.github.com>
2023-08-19 01:33:03 +0200

    list recipe section added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
-
+export class RecipeListComponent implements OnInit {
+  recipes: Array<Recipe> = [
+    new Recipe(
+      'Mango Shake',
+      '1 glass of milk and 1 mango',
+      'https://cdn.pixabay.com/photo/2016/04/21/18/53/mango-1344270_1280.jpg'
+    ),
+  ];
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
